172bbd1650e6b90e2d088850ea8b7a3c604038e4
hoomanist <hham.group12@gmail.com>
2020-08-10 18:56:55 +0430

    cors is good now

--- server/app.py
+++ server/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 from utils import Generate_token, JSONEncoder, Hash, EmailValidation, usernameNotRepetitious
 from datetime import datetime
+
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/foodly"
 app.json_encoder = JSONEncoder
 mongo = PyMongo(app)
+CORS(app)
 
 @app.route("/register", methods=['POST'])
 def register():
